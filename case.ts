31550f928bf16d891afb22c181d313c4165c01af
ALYANSHEIKHH <alyansheikhh123@gmail.com>
2024-03-22 10:51:42 +0000

    push my cases to git hub

---
+++ case.ts
@@ -0,0 +1,5 @@
+let namecase: string = "Alyan";
+console.log(namecase.toLowerCase());//Shows the name in all small Letters
+console.log(namecase.toUpperCase());//shows the name in ALL BIG LETTERS
+console.log(namecase.charAt(0).toUpperCase()+ namecase.slice(1).toLowerCase());//shows the name with the First Letter Big
+
